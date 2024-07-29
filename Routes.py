e75604d3d5eaf5fda470d5e380a672f6c15f4c40
bashafathima <168980781+bashafathima@users.noreply.github.com>
2024-07-29 12:05:29 +0530

    Update Routes.py

--- Routes.py
+++ Routes.py
@@ -1,4 +1,4 @@
-import psycopg2
+import psycopg2,os,time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -6,9 +6,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import numpy as np
 
+os.system('cls')
 data = [
+     ['https://www.redbus.in/online-booking/apsrtc/?utm_source=rtchometile','APSRTC'],
+    ['https://www.redbus.in/online-booking/ksrtc-kerala/?utm_source=rtchometile','KSRTC'],
+    ['https://www.redbus.in/online-booking/tsrtc/?utm_source=rtchometile','TSRTC'],
 ['https://www.redbus.in/online-booking/ktcl/?utm_source=rtchometile','KTCL']
 ,['https://www.redbus.in/online-booking/rsrtc/?utm_source=rtchometile','RSRTC'],
 ['https://www.redbus.in/online-booking/south-bengal-state-transport-corporation-sbstc/?utm_source=rtchometile','SBSTC'],
@@ -42,11 +47,44 @@ for i in list_from_numpy:
     options= webdriver.ChromeOptions()
     driver=webdriver.Chrome()
     driver.get(url)
-
+    action=ActionChains(driver)
+    page =[]
+    wait=WebDriverWait(driver,20)
     try:
-        div_elements = driver.find_elements(By.CLASS_NAME, 'route_link')
-        # Loop through each div element
-        Extract_Routes(div_elements)
+        WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.CLASS_NAME, 'DC_117_paginationTable')))
+        
+        # Find all pagination items (page numbers)
+        pagination_items = driver.find_elements(By.CLASS_NAME, 'DC_117_paginationTable')
+        
+        js_add_class = """
+        var element = document.querySelector(".DC_117_pageTabs");
+        if (element) {
+            element.classList.add(" DC_117_pageActive");
+        }
+        """
+        for index, element in enumerate(pagination_items):
+            result = element.text.split('\n')
+        for i in range(len(result)):
+            try:
+                page_tab_text = result[i]
+                if(page_tab_text=='1'):
+                    div_elements = driver.find_elements(By.CLASS_NAME, 'route_link')
+                    Extract_Routes(div_elements)
+                else:
+                    page= wait.until(EC.presence_of_element_located((By.XPATH,'//*[@class="DC_117_paginationTable"]')))
+                    driver.execute_script("arguments[0].scrollIntoView();", page)
+                    next=page.find_element(By.XPATH,f'//div[@class="DC_117_pageTabs " and text()={page_tab_text}]')
+                    time.sleep(10)
+                    next.click()
+                    div_elements = driver.find_elements(By.CLASS_NAME, 'route_link')
+                    Extract_Routes(div_elements)
+                
+               
+            except Exception as e:
+                print(f"No more pages or an error occurred: {e}")
+                break
+        
+        
     finally:
         # Close the WebDriver session
         driver.quit()
